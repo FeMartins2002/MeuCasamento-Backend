9bcd489ca7a8cefea2170f6299a049b70bd1cf4e
Felipe <felipesj10@hotmail.com>
2026-02-03 07:54:27 -0300

    :sparkles: Cria entidade Budget

---
+++ src/main/java/br/com/MeuCasamento/entities/Budget.java
@@ -0,0 +1,82 @@
+package br.com.MeuCasamento.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Budget implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "Generation_Date")
+    private Date generationDate;
+
+    @Column(name = "Total_Value")
+    private double totalValue;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "Party_ID", nullable = false, unique = true)
+    private Party party;
+
+    @OneToOne(mappedBy = "budget")
+    private Payment payment;
+
+    public Budget() {
+
+    }
+
+    public Budget(Date generationDate, double totalValue, Party party, Payment payment) {
+        this.generationDate = generationDate;
+        this.totalValue = totalValue;
+        this.party = party;
+        this.payment = payment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Date getGenerationDate() {
+        return generationDate;
+    }
+
+    public void setGenerationDate(Date generationDate) {
+        this.generationDate = generationDate;
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public Party getParty() {
+        return party;
+    }
+
+    public void setParty(Party party) {
+        this.party = party;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+}
