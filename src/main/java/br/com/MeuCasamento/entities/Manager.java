86b78ed77f65ad0408629b7064ba1ff6cc8c3388
Felipe <felipesj10@hotmail.com>
2026-02-04 11:05:14 -0300

    :sparkles: Cria entidade Manager

--- src/main/java/br/com/MeuCasamento/entities/Manager.java
+++ src/main/java/br/com/MeuCasamento/entities/Manager.java
@@ -1,5 +1,133 @@
 package br.com.MeuCasamento.entities;
 
-public class Manager {
+import br.com.MeuCasamento.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Managers")
+public class Manager implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "E-mail")
+    private String email;
+
+    @Column(name = "Phone")
+    private String phone;
+
+    @Column(name = "CPF")
+    private String cpf;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "Role")
+    private Role role;
+
+    @Column(name = "Password")
+    private String password;
+
+    @OneToMany(mappedBy = "manager")
+    private List<Party> parties = new ArrayList<>();
+
+    @OneToMany(mappedBy = "manager")
+    private List<Professional> professionals = new ArrayList<>();
+
+    @OneToMany(mappedBy = "manager")
+    private List<Service> services = new ArrayList<>();
+
+    @OneToMany(mappedBy = "manager")
+    private List<PartyHall> partyHalls = new ArrayList<>();
+
+    public Manager() {
+
+    }
+
+    public Manager(String name, String email, String phone, String cpf, String address, String password) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.cpf = cpf;
+        this.address = address;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Party> getParties() {
+        return parties;
+    }
+
+    public List<Professional> getProfessionals() {
+        return professionals;
+    }
+
+    public List<Service> getServices() {
+        return services;
+    }
+
+    public List<PartyHall> getPartyHalls() {
+        return partyHalls;
+    }
 }
